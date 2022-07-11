bd7b33ed639b477581b2976ee274f6d5e2381ce6
Pankaj Vatwani <vatwanipankaj74@gmail.com>
2022-07-11 17:03:54 +0530

    TODO

--- src/firebase.js
+++ src/firebase.js
@@ -3,14 +3,13 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  databaseURL: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
-  measurementId: "",
+  apiKey: "AIzaSyAXtXjlVytiAyZ1WTzG1iuNc2koxI7iUrw",
+  authDomain: "todo-app-199a4.firebaseapp.com",
+  projectId: "todo-app-199a4",
+  storageBucket: "todo-app-199a4.appspot.com",
+  messagingSenderId: "827953155826",
+  appId: "1:827953155826:web:bf13bd8a1aa25169684a6b",
+  measurementId: "G-JJV5VMXWFS"
 };
 
 const app = initializeApp(firebaseConfig);
